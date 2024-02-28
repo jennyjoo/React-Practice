23425435baefee4d84e42fa357b8ae785271e74b
yonjoos <joyce977@naver.com>
2024-02-28 21:40:37 +0900

    initial settings done

---
+++ rbvite/.eslintrc.cjs
@@ -0,0 +1,26 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'plugin:react/recommended',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:jsx-a11y/recommended',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs', 'jsx-a11y'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react-refresh'],
+  rules: {
+    quotes: ['error', 'single', { allowTemplateLiterals: true }],
+
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+  },
+  settings: {
+    react: { version: 'detect' }, // for react version warnning
+  },
+};
